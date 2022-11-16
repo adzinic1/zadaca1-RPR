7eb4d07730aef902c702849805fcec6409fb0367
Ajla <adzinic1@etf.unsa.ba>
2022-11-16 14:00:32 +0100

    Prepravka println

--- src/main/java/ba/unsa/etf/rpr/StdOut.java
+++ src/main/java/ba/unsa/etf/rpr/StdOut.java
@@ -1,9 +1,25 @@
 package ba.unsa.etf.rpr;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 
 
-public class StdOut {
-       public static void println(Double pop) {
+public class StdOut{
 
+    private static String unos;
+    private static PrintWriter out;
+
+    static{
+        try {
+            out = new PrintWriter(new OutputStreamWriter(System.out, unos), true);
+        }
+        catch (UnsupportedEncodingException e) {
+            System.out.println();
+        }
+    }
+
+    public static void println(Double pop) {
+        out.println();
     }
 }
