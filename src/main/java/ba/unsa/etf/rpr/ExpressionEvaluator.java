1917be724f8b5779876e69db933e2ad00eabd304
Ajla <adzinic1@etf.unsa.ba>
2022-11-19 16:49:25 +0100

    Prati se nepravilan broj zagrada

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -1,11 +1,34 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Objects;
 import java.util.Stack;
 
 public class ExpressionEvaluator {
+
+    public static int brojZagrada(String[] args){
+        int brojacOtvorene=0;
+        int brojacZatvorene=0;
+        for (int i = 0 ; i < args.length ; i++) {
+            if (args[i].equals("(")) brojacOtvorene = 1 + brojacOtvorene;
+            if (args[i].equals(")")) brojacZatvorene = 1 + brojacZatvorene;
+            }
+        if(brojacOtvorene==0 && brojacZatvorene==0) return 2;
+        if(Objects.equals(brojacOtvorene, brojacZatvorene)) return 1;
+
+        return 0;
+    }
+
     public static Stack<Double> evaluate(String[] args) {
         Stack<String> ops = new Stack<String>();
         Stack<Double> vals = new Stack<Double>();
+
+            try {
+                if (brojZagrada(args) == 0 || brojZagrada(args) == 2 ) throw new RuntimeException("Uneseni izraz nije aritmetički validan.");
+            }catch (RuntimeException e){
+                throw new RuntimeException("Uneseni izraz nije aritmetički validan.");
+            }
+
+
 
         for (int i = 0 ; i < args.length ; i++) {
             if (args[i].equals("("));
@@ -21,15 +44,14 @@ public class ExpressionEvaluator {
 
                 String op = ops.pop();
                 double v = vals.pop();
-
                 if(op.equals("+")){
                     v = vals.pop() + v;
                 } else if(op.equals("-")){
                     v = vals.pop() - v;
                 } else if(op.equals("*")) {
-                    v = vals.pop() - v;
+                    v = vals.pop() * v;
                 } else if(op.equals("/")) {
-                    v = vals.pop() - v;
+                    v = vals.pop() / v;
                 }
                 vals.push(v);
             }
@@ -37,6 +59,7 @@ public class ExpressionEvaluator {
                 vals.push(Double.parseDouble(args[i]));
             }
         }
+
         return vals;
     }
     }
